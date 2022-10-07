acb24ef218c85dcadf902ac7e093f63d2bc30924
Márcio Tenório <marciotenorioj@gmail.com>
2022-10-07 13:20:41 -0300

    introducing scenarios params and log

--- src/main/java/request/Main.java
+++ src/main/java/request/Main.java
@@ -1,7 +1,16 @@
 package request;
 
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) throws Exception{
+        Logger logger = Logger.getLogger("main");
+
+        logger.info(LocalDateTime.now().toString());
+
         org.openjdk.jmh.Main.main(args);
+
+        logger.info(LocalDateTime.now().toString());
     }
 }
--- src/main/java/request/MyIPSequentialBenchmark.java
+++ src/main/java/request/MyIPSequentialBenchmark.java
@@ -4,15 +4,21 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
+@State(Scope.Benchmark)
 public class MyIPSequentialBenchmark {
+
+    @Param({"10", "50", "100", "250", "500"})
+    public int iterations;
+
+
     @org.openjdk.jmh.annotations.Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @Fork(value = 20, warmups = 1)
+    @Fork(1)
     @Warmup(iterations = 2)
-    @Measurement(iterations = 2)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void init(){
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < iterations; i++) {
             Requester requester = new Requester();
             requester.run();
         }
